d31e1fafe684c3cc720311f4a8cf9663406cccef
Ann Fadeeva <haineko1512@mail.ru>
2015-05-26 23:12:29 +0300

    Удалила Gruntfile.js и добавила gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+var reload      = browserSync.reload;
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function() {
+  return gulp.src("sass/style.scss")
+    .pipe(sass())
+	.pipe(autoprefixer())
+    .pipe(gulp.dest("css"))
+    .pipe(reload({stream: true}));
+});
+
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+    server: '.'
+  });
+
+  gulp.watch("sass/**/*.scss", ['sass']);
+  gulp.watch("mobile-sedona/*.html").on('change', reload);
+});
+
+gulp.task('default', ['serve']);
